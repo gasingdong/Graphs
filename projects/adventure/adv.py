ea497a4cc6c7d7f475fc8f761a2cb18a8840543f
Brandon Dong <gasingdong@gmail.com>
2020-03-19 10:21:10 -0700

    clean up trial logic

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -34,13 +34,13 @@ traversal_path = []
 
 
 def init_db(rooms, db):
-    size = len(world.rooms)
+    size = len(rooms)
     paths = {}
     for start in range(size):
         print(f"{size - start}...")
-        start_room = world.rooms[start]
+        start_room = rooms[start]
         for dest in range(size):
-            dest_room = world.rooms[dest]
+            dest_room = rooms[dest]
             room_paths = {start_room.id: [start_room.id]}
             q = deque()
             q.append(start_room)
@@ -64,53 +64,57 @@ with shelve.open('db') as db:
         init_db(world.rooms, db)
     db_paths = db['paths']
 
-trials = 100
-shortest_traversal = None
-for _ in range(trials):
-    temp_traversal = []
-    current_room = world.starting_room.id
-    tsp = [current_room]
-    visited = {current_room}
 
-    while len(visited) < len(room_graph):
-        nearest_neighbor = None
-        current_shortest = None
-        current_dict = db_paths[current_room]
-        for room_id in current_dict:
-            if room_id not in visited:
-                if (room_id != current_room and
-                    (not current_shortest or
-                     (len(current_dict[room_id]) <= current_shortest))):
-                    if len(current_dict[room_id]) == current_shortest:
-                        if random.randint(0, 1) == 1:
-                            current_shortest = len(current_dict[room_id])
-                            nearest_neighbor = room_id
-                    else:
-                        current_shortest = len(current_dict[room_id])
-                        nearest_neighbor = room_id
-        tsp.append(nearest_neighbor)
-        visited.add(nearest_neighbor)
-        current_room = nearest_neighbor
+def get_nearest_neighbor(current_room_id, paths, visited, greed=1):
+    candidates = []
+    for room_id in [_ for _ in paths if _ != current_room_id]:
+        if room_id not in visited:
+            candidates.append((room_id, len(paths[room_id])))
+    candidates.sort(key=(lambda x: x[1]))
+    nearest_neighbor = random.choice(candidates[:greed])[0]
+    return nearest_neighbor
 
-    full_path = [0]
 
-    for i in range(len(tsp) - 1):
-        current_room = tsp[i]
-        next_room = tsp[i + 1]
-        shortest_path = db_paths[current_room][next_room][1:]
-        full_path = full_path + shortest_path
-
-    for i in range(len(full_path) - 1):
-        current_room = world.rooms[full_path[i]]
-        next_room_id = full_path[i + 1]
+def add_directions_from_path(rooms, path, traversal):
+    for i in range(len(path) - 1):
+        current_room = rooms[path[i]]
+        next_room_id = path[i + 1]
         for ex in current_room.get_exits():
             room = current_room.get_room_in_direction(ex)
             if room and room.id == next_room_id:
-                temp_traversal.append(ex)
-    if not shortest_traversal or len(temp_traversal) < len(shortest_traversal):
-        shortest_traversal = temp_traversal
+                traversal.append(ex)
 
-traversal_path = shortest_traversal
+
+trials = 10000
+greed = 2
+shortest_path = None
+
+for _ in range(trials):
+    current_room_id = world.starting_room.id
+    current_path = [current_room_id]
+    visited = {current_room_id}
+
+    while len(visited) < len(room_graph):
+        nearest_neighbor_id = get_nearest_neighbor(
+            current_room_id, db_paths[current_room_id], visited, greed)
+        current_path.append(nearest_neighbor_id)
+        visited.add(nearest_neighbor_id)
+        current_room_id = nearest_neighbor_id
+
+    # Expand path to full list of room ids to visit
+    expanded_path = [0]
+    for i in range(len(current_path) - 1):
+        current_room = current_path[i]
+        next_room = current_path[i + 1]
+        expanded_path += db_paths[current_room][next_room][1:]
+
+    if (not shortest_path
+            or len(expanded_path) < len(shortest_path)):
+        shortest_path = expanded_path
+
+
+# Turn path into list of directions
+add_directions_from_path(world.rooms, shortest_path, traversal_path)
 print(traversal_path)
 
 # TRAVERSAL TEST
